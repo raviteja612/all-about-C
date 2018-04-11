61cb84834aa1dc0300a414f0449a368b0a1b3ffe
raviteja612 <raviteja612@gmail.com>
2018-04-11 01:11:26 -0700

    updated malloc and null pointers

--- concatStrings.c
+++ concatStrings.c
@@ -1,6 +1,10 @@
 /*
 allocate heap memory inside the function call and return that to the main function.
 Using malloc will allocate the heap memory inside the called function and will persist in the main function when it is returned.
+
+Malloc: allocates the memory of generic type and typecasts it based on what you mention. This is unitialized. To initialize to all 
+zeros, use calloc. A NULL pointer is returned if there is no memory available. A NULL pointer is basically something pointing to 
+an address whose value is zero. An address location at 0 is always invalid. It needs to be > 0.
 */
 
 
