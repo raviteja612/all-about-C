f253ff6cb9fc22d096ad7f37b0bd80d74b36624d
Ravi Teja <raviteja.612@gmail.com>
2018-04-11 21:30:58 -0700

    test file commit

---
+++ pointerToStructs.c
@@ -0,0 +1,32 @@
+/*This program summarizes how you typedef or define a structure. Use a pointer to such a structure.
+Allocate memory to that pointer so that you can define values to those structure members.
+you can use other pointers to access these defined members i.e. using a different pointer to point to these defined members.
+Display all the addresses and values of each structure members
+
+struct [structure tag] {
+   member definition;
+   member definition;
+   ...
+   member definition;
+} [one or more structure variables];
+
+or
+typedef struct {
+
+   member definition;
+   member definition;
+   ...
+   member definition;
+} [structure tag] ;
+
+[structure tag] [one or more structure variables]; 
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(void)
+{
+
+}
